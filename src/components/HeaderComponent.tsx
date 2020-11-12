3cd5239aa067e99c815ddb5d896b5eff83fec19d
matt bimpson <m.bimpson@gmail.com>
2020-11-12 12:47:04 -0500

    basic structure with routing

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
+import { Container } from './HeaderComponentStyles';
 
 const Header: React.FC<any> = () => {
   return (
     <>
+      <Container>
+        
+      </Container>
     </>
   )
 }
---
+++ src/components/HeaderComponentStyles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Container: any = styled.div`
+  width: 100%;
+  position: sticky;
+  top: 0;
+  padding: 20px;
+  background-color: blue;
+`;
