1e4c77aa908f3ecaab056b75a892a58626dd6f21
Matt Bimpson <matt@Matts-MacBook-Pro.local>
2020-11-12 12:28:59 -0500

    initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const HomePage: React.FC<any> = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default HomePage;
---
+++ src/pages/Work.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const WorkPage: React.FC<any> = () => {
+  return (
+    <>
+    </>
+  )
+}
+
+export default WorkPage;
