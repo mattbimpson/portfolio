81b570890a237266329a44ffc5146f3f8e2ffced
matt bimpson <m.bimpson@gmail.com>
2020-11-13 11:58:13 -0500

    darken text background

--- src/pages/home/Styles.ts
+++ src/pages/home/Styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container =
   styled.div`
-    background-color: rgba(0, 0, 0, 0.5);
+    background-color: rgba(0, 0, 0, 0.7);
     padding: 20px;
     width: 800px;
     margin: 0 auto;
